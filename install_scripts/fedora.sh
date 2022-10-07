d50d6e026731901ade0ea6d63b29ef0080946ccb
Salvador Gudino <cero@duck.com>
2022-10-07 01:39:25 -0500

    Add fedora package maintainer tools

--- install_scripts/fedora.sh
+++ install_scripts/fedora.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 sudo dnf install -y \
-  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm
 
 sudo dnf install -y \
-  https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+  https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm
 
 sudo dnf install -y @Multimedia \
 @virtualization \
@@ -43,4 +43,6 @@ gnome-shell-extension-appindicator \
 vim \
 steam \
 rust \
-cargo
+cargo \
+fedora-packager \
+fedora-review
