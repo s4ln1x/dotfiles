4c37f2aec288551b6c0ce0d9cd5136d63842fd9e
Salvador Gudino <44090859+s4ln1x@users.noreply.github.com>
2025-07-16 06:30:59 -0600

    Split fedora script based on architecture

---
+++ scripts/fedora_aarch64.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+# Install rpmfusion mirrors
+sudo dnf install -y \
+    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm \
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm
+
+sudo dnf install -y \
+@development-tools \
+@virtualization \
+ShellCheck \
+akmod-nvidia \
+cargo \
+cockpit \
+cockpit-machines \
+cockpit-podman \
+cockpit-session-recording \
+crontabs \
+dconf-editor \
+fd-find \
+fedora-packager \
+fedora-review \
+fira-code-fonts \
+firefox \
+gcc-c++ \
+gimp \
+gnome-extensions-app \
+gnome-shell-extension-appindicator \
+gnome-shell-extension-pomodoro \
+gnome-tweaks \
+golang \
+htop \
+inotify-tools \
+jetbrains-mono-fonts-all \
+libvirt \
+lynis \
+meld \
+nodejs \
+npm \
+nvidia-gpu-firmware \
+openssh \
+peek \
+powerline-fonts \
+rpmconf \
+rust \
+telegram-desktop \
+tmux \
+trash-cli \
+util-linux-user \
+vgrep \
+vim \
+virt-manager \
+wimlib-utils \
+wordnet \
+xclip \
+xorg-x11-drv-nvidia-cuda \
+xsel \
+zsh
